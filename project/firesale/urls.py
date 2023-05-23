d7d87134f157b38ee5f3962419bbdd6945839b0f
sunnakaradottir <94842398+sunnakaradottir@users.noreply.github.com>
2023-05-23 21:20:48 +0000

    changing the setup to match lectures

--- project/firesale/urls.py
+++ project/firesale/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('members/', views.get_members, name='members'),
-    path('members/create/', views.create_member, name='create_member'),
-    path('items/', views.get_items, name='items'),
-    path('items/create/', views.create_item, name='create_item'),
+    path('create_member/', views.create_member, name='create_member'),
+    path('create_item/', views.create_item, name='create_item'),
 ]
