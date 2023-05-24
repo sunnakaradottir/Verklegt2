4db2565e4b2c4b017b5a5755e3550e3807f5024b
sunnakaradottir <94842398+sunnakaradottir@users.noreply.github.com>
2023-05-24 13:41:32 +0000

    user profile ready without styling

---
+++ project/user/forms/profile_form.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm, widgets
+from user.models import Profile
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['id', 'user']
+        widgets = {'name': widgets.TextInput(attrs={'class': 'form-control'}),'favorite_candy': widgets.Select(attrs={'class': 'form-control'}), 'profile_image': widgets.TextInput(attrs={'class': 'form-control'})}
