8812fa0ada95dd1118af01bb2774a8b7e1914c62
Dana Tryggva <dana@programm.is>
2023-05-24 22:02:02 +0000

    lagaði smá styling

---
+++ project/static/js/index.js
@@ -0,0 +1,6 @@
+
+    function show_item(this_item) {
+        // Code to run when the element is clicked
+        console.log("Blesssssss");
+        // Add your logic here to display more information or perform other actions
+      }
