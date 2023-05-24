25c9a202b898e12c5ebc0a691913afbe0510985a
sunnakaradottir <94842398+sunnakaradottir@users.noreply.github.com>
2023-05-24 19:18:29 +0000

    delete item is now working but is missing authentication checks

--- project/firesale/urls.py
+++ project/firesale/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create_member/', views.create_member, name='create_member'),
     path('create_item/', views.create_item, name='create_item'),
     path('item/<slug:item_id>/', views.item_information, name='item_information'),
+    path('items/delete/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
