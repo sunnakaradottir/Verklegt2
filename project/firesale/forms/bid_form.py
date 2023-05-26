dd9048c75234fcf2752478a89f0be2e79e64707f
sunnakaradottir <94842398+sunnakaradottir@users.noreply.github.com>
2023-05-26 11:21:29 +0000

    adding placeholder in bid creation to remind us of valid inputs when testing

--- project/firesale/forms/bid_form.py
+++ project/firesale/forms/bid_form.py
@@ -5,6 +5,5 @@ from django import forms
 class BidForm(ModelForm):
     class Meta:
         model = Bid
-        exclude = ['id', 'item', 'user']
-        widgets = {'bid_amount': widgets.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your bid'})}
-        fields = ["bid_amount"]
+        exclude = ['id', 'item', 'user', 'status']
+        widgets = {'bid_amount': widgets.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your bid (Numerical input between -2147483648 and 2147483647)'})}
