9fb699981e69f1befabc429ed1b6181a395d2e3a
Halldóra Gísladóttir <halldorag20@ru.is>
2023-05-27 16:12:35 +0000

    Trying to fix the search, looks in the console right but is not showing the right info on the page it self

--- project/firesale/views.py
+++ project/firesale/views.py
@@ -12,22 +12,22 @@ from .models import Item, ItemImage
 
 
 # Create your views here
-def index(request):
-    if 'search_filter' in request.GET:
-        search_filter = request.GET['search_filter']
-        items = []
-        itemimages = ItemImage.objects.all()
-        print(Item.objects.filter(name__icontains='Mirror'))
-        for item_found in Item.objects.filter(name__icontains=search_filter):
-            for itemimage in itemimages:
-                if itemimage.item == item_found:
-                    items.append({'id': item_found.id, 'name': item_found.name, 'image': itemimage.img_url,
-                                  'description': item_found.description})
-        return JsonResponse({'data': items})
-    return render(request, "items/index.html",
-                  {"items": models.Item.objects.all(), "itemimages": models.ItemImage.objects.all(),
-                   'include_item_information': True, })
 
+def index(request):
+     if 'search_filter' in request.GET:
+         search_filter = request.GET['search_filter']
+         items = []
+         itemimages = ItemImage.objects.all()
+         print(Item.objects.filter(name__icontains='Mirror'))
+         for item_found in Item.objects.filter(name__icontains=search_filter):
+             for itemimage in itemimages:
+                 if itemimage.item == item_found:
+                     items.append({'id': item_found.id, 'name': item_found.name, 'image': itemimage.img_url,
+                                   'description': item_found.description})
+         return JsonResponse({'data': items})
+     return render(request, "items/index.html",
+                   {"items": models.Item.objects.all(), "itemimages": models.ItemImage.objects.all(),
+                   'include_item_information': True, })
 
 def get_members(request):
     return render(
