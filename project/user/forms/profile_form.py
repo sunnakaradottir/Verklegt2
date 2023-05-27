868745b5a4fea3a5a0eff6196003a43a1d7c4aee
Dana Tryggva <dana@programm.is>
2023-05-27 18:11:27 +0000

    bætti bio við user profileinn

--- project/user/forms/profile_form.py
+++ project/user/forms/profile_form.py
@@ -5,4 +5,7 @@ class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         exclude = ['id', 'user']
-        widgets = {'name': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your full name'}),'favorite_candy': widgets.Select(attrs={'class': 'form-control'}), 'profile_image': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a URL for your profile image'})}
+        widgets = {'name': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your full name'}),
+                   'bio': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Tell us a little bit about yourself'}),
+                   'favorite_item': widgets.Select(attrs={'class': 'form-control'}), 
+                   'profile_image': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a URL for your profile image'})}
--- project/user/models.py
+++ project/user/models.py
@@ -6,5 +6,6 @@ from firesale.models import Item
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.CharField(max_length=150, blank=True, null=True)
     favorite_item = models.ForeignKey(Item, on_delete=models.SET_NULL, blank=True, null=True, related_name='favorite_item')
     profile_image = models.CharField(max_length=9999, blank=True, null=True)
