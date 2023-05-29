4d3f5f5720dd087ac391c98fe9710bb416a04dbf
sunnakaradottir <94842398+sunnakaradottir@users.noreply.github.com>
2023-05-29 15:13:11 +0000

    fixing errors on forms

--- project/firesale/forms/orderreview_form.py
+++ project/firesale/forms/orderreview_form.py
@@ -6,4 +6,3 @@ class OrderReviewForm(ModelForm):
     class Meta:
         model = models.Order
         exclude = ["id", "item", "buyer", "seller", "creation_time", "contact", "payment"]
-        widgets = {}
