e34ac3b20d8fb64e7b8ccf2ad8bed83feb06499f
Halldóra Gísladóttir <halldorag20@ru.is>
2023-05-31 20:50:03 +0000

    fixing the search engine

--- project/static/js/items.js
+++ project/static/js/items.js
@@ -1,44 +1,46 @@
 $(document).ready(function(){
-    $('#search-btn').on( 'click',  function(e){
+    $('#search-btn').on('click', function(e){
         e.preventDefault();
-        let searchText = $('#search-box').val();
+        let searchText = $('#search-box').val().trim();  // Trim leading and trailing whitespaces
+        if (searchText === '') {
+            return;  // If search text is empty, do nothing
+        }
         console.log(searchText)
         $.ajax({
-            url: '/?search_filter='+ searchText,
+            url: '/?search_filter=' + searchText,
             type: 'GET',
             success: function(resp) {
-                console.log(resp.data);
-                let newHtml = resp.data.map(d => {
-                    let priceHtml = d.price === null ? '0' : d.price;
-
-
-                    return `<a class="single_item" href="/item/${d.id}">
-                        <div class="name">${d.name}</div>
-                        <div class="item_content">
-                            <div class="item_content" id="single_item_image">
-                            
-                                <div class="image_container">
-                                    <img src="${d.image}" width="200px" height="200px" alt="Image of ${d.name}" />
-                                </div>
-                           </div>
-                           
-                        </div>
-                        <div class="item_information"> Category: ${d.category}</div>
-                        <div class="item_information"> Condition: ${d.condition}</div>
-                         <div class="item_information"> Location: ${d.item_location}</div>
-                       <div class="item_content">
-                            <div class="item_information" id="price">Starting price: $ ${priceHtml}</div>
-                       </div>
-                          
-                    </a>`;
-                });
-                $('.all_items').html(newHtml.join(''));
+                if (resp.data && resp.data.length > 0) {
+                    console.log(resp.data);
+                    let newHtml = resp.data.map(d => {
+                        let priceHtml = d.price === null ? '0' : d.price;
+                        return `<a class="single_item" href="/item/${d.id}">
+                            <div class="name">${d.name}</div>
+                            <div class="item_content">
+                                <div class="item_content" id="single_item_image">
+                                    <div class="image_container">
+                                        <img src="${d.image}" width="200px" height="200px" alt="Image of ${d.name}" />
+                                    </div>
+                               </div>
+                            </div>
+                            <div class="item_information">Category: ${d.category}</div>
+                            <div class="item_information">Condition: ${d.condition}</div>
+                            <div class="item_information">Location: ${d.item_location}</div>
+                            <div class="item_content">
+                                <div class="item_information" id="price">Starting price: $ ${priceHtml}</div>
+                            </div>
+                        </a>`;
+                    });
+                    $('.all_items').html(newHtml.join(''));
+                } else if (resp.message) {
+                    $('.all_items').html(`<div class="no_items">${resp.message}</div>`);
+                }
                 $('#search-box').val('');
             },
             error: function(xhr, status, error){
-                //TODO: SHOW toastr
+                // TODO: SHOW toastr
                 console.error(error);
             }
-            })
-        });
+        })
+    });
 });
