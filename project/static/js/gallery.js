048027cff13c4f10b768be5196a93f9d85b31fbe
Dana Tryggva <dana@programm.is>
2023-05-31 17:59:31 +0000

    laga css á single page

--- project/static/js/gallery.js
+++ project/static/js/gallery.js
@@ -13,7 +13,7 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "flex";
 }
 
 function plusSlides(n) {
