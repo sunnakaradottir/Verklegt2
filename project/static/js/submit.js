90fb5422bc430272d66c33a5b78e80dd99363e11
sunnakaradottir <94842398+sunnakaradottir@users.noreply.github.com>
2023-06-01 20:14:55 +0000

    modifying js to prevent error on initial load

--- project/static/js/submit.js
+++ project/static/js/submit.js
@@ -1,20 +1,21 @@
 
 let btn = document.getElementById('button_enter');
     
-    // when the btn is clicked print info in console 
-    btn.addEventListener('click', (ev)=>{
-      console.log("Btn clicked");
-    });
- 
-    document.addEventListener('keypress', (event)=>{
- 
-      // event.keyCode or event.which  property will have the code of the pressed key
-      let keyCode = event.keyCode ? event.keyCode : event.which;
- 
-      // 13 points the enter key
-      if(keyCode === 13) {
-        // call click function of the buttonn 
-        btn.click();
-      }
-        
-    });
+// when the btn is clicked print info in console 
+
+if(btn) {
+  btn.addEventListener('click', (ev)=>{
+    console.log("Btn clicked");
+  });
+   
+  document.addEventListener('keypress', (event)=>{
+    // event.keyCode or event.which  property will have the code of the pressed key
+    let keyCode = event.keyCode ? event.keyCode : event.which;
+   
+    // 13 points the enter key
+    if(keyCode === 13) {
+      // call click function of the buttonn 
+      btn.click();
+    }
+  });
+}
