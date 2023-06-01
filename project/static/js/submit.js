3d9292c4dc135bd955f4311a5152b539a31afd17
Dana Tryggva <dana@programm.is>
2023-06-01 20:09:47 +0000

    bæta við enter virkni

---
+++ project/static/js/submit.js
@@ -0,0 +1,20 @@
+
+let btn = document.getElementById('button_enter');
+    
+    // when the btn is clicked print info in console 
+    btn.addEventListener('click', (ev)=>{
+      console.log("Btn clicked");
+    });
+ 
+    document.addEventListener('keypress', (event)=>{
+ 
+      // event.keyCode or event.which  property will have the code of the pressed key
+      let keyCode = event.keyCode ? event.keyCode : event.which;
+ 
+      // 13 points the enter key
+      if(keyCode === 13) {
+        // call click function of the buttonn 
+        btn.click();
+      }
+        
+    });
