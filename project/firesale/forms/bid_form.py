06a9e44a0fe28a090b49325d54c38eb66affb676
sunnakaradottir <94842398+sunnakaradottir@users.noreply.github.com>
2023-06-02 17:44:04 +0000

    no negative bid amounts and new email address

--- project/firesale/forms/bid_form.py
+++ project/firesale/forms/bid_form.py
@@ -1,9 +1,10 @@
 from django.forms import ModelForm, widgets
 from firesale.models import Bid
-from django import forms
+from django.core.validators import MinValueValidator
 
 class BidForm(ModelForm):
     class Meta:
         model = Bid
         exclude = ['id', 'item', 'user', 'status']
         widgets = {'bid_amount': widgets.NumberInput(attrs={'class': 'form-control', 'min': 0, 'placeholder': 'Enter your bid'})}
+        validators=[MinValueValidator(1)]
